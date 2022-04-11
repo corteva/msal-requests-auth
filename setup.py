d50c5ab86c2f35b27cffc6b32006804fd70990cb
Alan D. Snow <alansnow21@gmail.com>
2022-04-11 08:28:16 -0500

    REF: Move requirements to setup.cfg (#9)

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
-from itertools import chain
-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 from msal_requests_auth import __version__
 
-requirements = ["msal", "requests", "pyperclip", "appdirs"]
-extras_require = {"dev": ["pytest", "pytest-cov"]}
-extras_require["all"] = list(chain.from_iterable(extras_require.values()))
-
-setup(
-    install_requires=requirements,
-    extras_require=extras_require,
-    packages=find_packages(include=["msal_requests_auth*"]),
-    version=__version__,
-)
+setup(version=__version__)
